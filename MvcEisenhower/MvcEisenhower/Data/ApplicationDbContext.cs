28ca22cd126f634eb377af09afc145d6c4a38d3e
Grzegorz Latuszek <grzegorz.latuszek@nokia.com>
2021-11-06 21:38:56 +0100

    TasksControler generated on Task Model
    via: Controllers / Add/ Controller
    MVC Ctrlr with EF + select Model + select AppDbContext

--- MvcEisenhower/MvcEisenhower/Data/ApplicationDbContext.cs
+++ MvcEisenhower/MvcEisenhower/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using MvcEisenhower.Models;
 
 namespace MvcEisenhower.Data
 {
@@ -12,5 +13,6 @@ namespace MvcEisenhower.Data
             : base(options)
         {
         }
+        public DbSet<MvcEisenhower.Models.Task> Task { get; set; }
     }
 }
