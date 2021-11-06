5cf1861253afe742f79f8a6bc87066c7a2e36635
Grzegorz Latuszek <grzegorz.latuszek@nokia.com>
2021-11-06 19:13:51 +0100

    Add Task Model

---
+++ MvcEisenhower/MvcEisenhower/Models/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MvcEisenhower.Models
+{
+	public class Task
+	{
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Boolean Important { get; set; }
+
+        public Boolean Urgent { get; set; }
+
+        public Task()
+		{
+
+		}
+	}
+}
