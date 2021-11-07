6b087d5b659db266ffd4a8a363663987f73ac827
Grzegorz Latuszek <grzegorz.latuszek@nokia.com>
2021-11-07 13:21:27 +0100

    Generated migration code (and DB)
    via Package Manager console commands:
    PM> add-migration "initialsetup"
    PM> update-database

---
+++ MvcEisenhower/MvcEisenhower/Data/Migrations/20211106204827_initialsetup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcEisenhower.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Task",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Important = table.Column<bool>(type: "bit", nullable: false),
+                    Urgent = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Task", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Task");
+        }
+    }
+}
