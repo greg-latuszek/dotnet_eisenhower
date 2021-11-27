18ea5e2dc685717d4ea6f1b2193bfa287d860524
Grzegorz Latuszek <grzegorz.latuszek@nokia.com>
2021-11-27 19:57:22 +0100

    Generate migration code (and update DB)
    via Package Manager console commands:
    PM> add-migration AddUsers
    PM> update-database

---
+++ MvcEisenhower/MvcEisenhower/Data/Migrations/20211127184950_AddUsers.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcEisenhower.Data.Migrations
+{
+    public partial class AddUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserID",
+                table: "Task",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Task_UserID",
+                table: "Task",
+                column: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Task_User_UserID",
+                table: "Task",
+                column: "UserID",
+                principalTable: "User",
+                principalColumn: "UserID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Task_User_UserID",
+                table: "Task");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Task_UserID",
+                table: "Task");
+
+            migrationBuilder.DropColumn(
+                name: "UserID",
+                table: "Task");
+        }
+    }
+}
