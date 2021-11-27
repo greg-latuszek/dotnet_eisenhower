7d37f4ca393441f62676066b6237c3dc4701683a
Grzegorz Latuszek <grzegorz.latuszek@nokia.com>
2021-11-27 18:58:40 +0100

    Generate migration code (and update DB)
    via Package Manager console commands:
    PM> add-migration RenameIdToTeamID
    PM> update-database
    
    see: https://docs.microsoft.com/en-us/ef/core/managing-schemas/migrations/managing?tabs=vs

---
+++ MvcEisenhower/MvcEisenhower/Data/Migrations/20211127174213_RenameIdToTeamID.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcEisenhower.Data.Migrations
+{
+    public partial class RenameIdToTeamID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Task",
+                newName: "TaskID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TaskID",
+                table: "Task",
+                newName: "Id");
+        }
+    }
+}
